cdebdaa6516fb44e2f1bcbc472415a04e245ec9a
miaow1988 <miaow1988@qq.com>
2018-11-10 19:55:32 +0800

    Improved codes.

--- dlfs/layers/__init__.py
+++ dlfs/layers/__init__.py
@@ -2,13 +2,16 @@ from .gradient_check import check_parameter_gradient
 from .gradient_check import check_bottom_gradient
 
 from .linear import Linear
+
 from .sigmoid import Sigmoid
 from .relu import ReLU
+
 from .cross_entropy_loss import CrossEntropyLoss
 from .softmax_loss import SoftmaxLoss
 
 __all__ = [
-    'check_parameter_gradient',
-    'check_bottom_gradient',
-    'Linear', 'Sigmoid', 'ReLU', 'CrossEntropyLoss', 'SoftmaxLoss'
+    'check_parameter_gradient', 'check_bottom_gradient',
+    'Linear',
+    'Sigmoid', 'ReLU',
+    'CrossEntropyLoss', 'SoftmaxLoss'
 ]
